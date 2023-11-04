81fc8fab5af9f69db054d9e25ed00ac22fd82da2
unknown <marcioh.dev@gmail.com>
2023-11-04 16:26:07 -0300

    Cadastro de usuário escrevendo no arquivo filmes.json, criado o método editar usuário (Ainda não está alterando o json).

--- config/rotas.js
+++ config/rotas.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const rotas = express.Router();
 const filmes = require("../src/data/filmes.json");
-const { adicionarFilme } = require("../src/controladores/filmesControlador") //Importando controlador Filmes
+const { adicionarFilme, editarFilme, listarFilme } = require("../src/controladores/filmesControlador") //Importando controlador Filmes
 
 
 // JSON FILMES (GET = BUSCAR OU SELECIONAR DADOS)
-rotas.get("/filmes", (requisicao, resposta) => {
-  // ROTA 'FILMES'
-  return resposta.json(filmes);
-});
+rotas.get("/filmes", listarFilme);
+
+//POST = INSERIR FILME
+rotas.post(`/filme`, adicionarFilme);
+
+//PUT - EDITAR FILME
+rotas.put('/filme/:id', editarFilme)
 
 // GET = BUSCA OU SELECIONAR DADOS
 rotas.get(`/filmes/filtrar/:titulo`, (requisicao, resposta) => {
   // ROTA PARA OBTER DADOS DOS USUARIOS
   return resposta.json(filmes);
 });
-
-//POST = INSERIR FILMES
-rotas.post(`/filme`, adicionarFilme);
 
 module.exports = rotas; //EXPORTANDO ROTAS
--- src/controladores/filmesControlador.js
+++ src/controladores/filmesControlador.js
@@ -1,26 +1,63 @@
-const filmes = require("../data/filmes.json")
-const Filme = require("../modelos/Filme")
+const filmes = require("../data/filmes.json") //Arquivo Json
+const Filme = require("../modelos/Filme") //Classe Filme.js
+const fs = require("fs/promises")
+
 let id = filmes.length + 2;
 
 function adicionarFilme(req, res) {
-    // ROTA PARA  ADICIONAR 'USUARIOS' AO ARRAY
+    try {
 
-    const { titulo, diretor, lancamento, genero, descricao, imagem, ondeAssistir } = req.body; // OBTENDO O CORPO DA SOLICITAÇÃO  JSON
+        // ROTA PARA  ADICIONAR 'USUARIOS' AO ARRAY
+        const { titulo, diretor, lancamento, genero, descricao, imagem, ondeAssistir } = req.body; // OBTENDO O CORPO DA SOLICITAÇÃO  JSON
 
-    const novoFilme = new Filme(id, titulo, diretor, lancamento, genero, descricao, imagem, ondeAssistir);
-    const adicionou = filmes.push(novoFilme); // ADICIONANDO UM NOVO DADO AO ARRAY
+        //validação que ve se o filme já existe
+        const filmeEncontrado = filmes.find(filme => filme.titulo === titulo && filme.diretor == diretor);
 
-    if (adicionou) {
-        id++;
-        return res.status(201).json({ mensagem: "Filme adicionado com sucesso", novoFilme }); // RESPOSTA COM ARRAY ATUALIZADO EM  JSON
+        if (filmeEncontrado) {
+            return res.json({ mensagem: 'Filme já está cadastrado.' })
+        }
+
+        const novoFilme = new Filme(id, titulo, diretor, lancamento, genero, descricao, imagem, ondeAssistir);
+        filmes.push(novoFilme);
+
+        fs.writeFile('./src/data/filmes.json', JSON.stringify(filmes));
+
+        return res.json({ mensagem: 'Filme cadastrado com sucesso.' })
+
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: "Erro do servidor. Filme não adicionado" })
+    }
+}
+
+function editarFilme(req, res) {
+
+    const { id } = req.params;
+    const { titulo, diretor, lancamento, genero, descricao, imagem, ondeAssistir } = req.body;
+
+    const filmeEncontrado = filmes.find((filme) => {
+        return filme.id === Number(id);
+    });
+
+    if (!filmeEncontrado) {
+        return res.status(404).json({ mensagem: 'Não existe filme a ser alterado para o ID informado.' });
     }
 
-    return res.status(500).json({ mensagem: "Erro do servidor. Filme não adicionado" })
+    filmeEncontrado.titulo = titulo
+    filmeEncontrado.diretor = diretor
+    filmeEncontrado.lancamento = lancamento
+    filmeEncontrado.genero = genero
+    filmeEncontrado.descricao = descricao
+    filmeEncontrado.imagem = imagem
+    filmeEncontrado.ondeAssistir = ondeAssistir
+
+    return res.json({ mensagem: 'Filme alterado', filmeEncontrado });
 
 }
 
-function editarFilme() {
+function listarFilme(req, res) {
 
+    return res.status(200).json(filmes)
 }
 
-module.exports = { adicionarFilme, editarFilme }
+module.exports = { adicionarFilme, editarFilme, listarFilme }
